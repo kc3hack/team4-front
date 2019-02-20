175cf9bd61ccd9f730543dfac0d26d9d4664b0ad
TATSUHIRO <>
2019-02-20 23:47:54 +0900

    install vuetifyjs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <v-card-title class="headline font-weight-regular blue-grey white-text"></v-card-title> -->
   </div>
 </template>
 
