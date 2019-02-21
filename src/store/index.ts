6886aa863cf25ffa3e0bd3a59a636f3b01cd4391
hideh1231 <hidekazunagamura@HidekazuNagamura.local>
2019-02-21 14:22:02 +0900

    commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,11 +4,12 @@ import axios from "axios";
 
 Vue.use(Vuex);
 
-const BASE_URL: string = "https://localhost:8080";
+const BASE_URL: string = "root:password@tcp(docker.for.mac.localhost:8080)";
 
 export default {
   modules: {
     anime: {
+      // animesのlist内の定義
       namespaced: true,
       state: {
         anime_id: 0,
@@ -35,6 +36,80 @@ export default {
             list: []
           };
           await axios.get(BASE_URL + "/animes").then(res => {
+            payload.list = res.data;
+          });
+          context.commit("getList", payload);
+        }
+      }
+    },
+    sanctuary: {
+      //sanctuariesのlist内の定義
+      namespaced: true,
+      state: {
+        sanc_id: 0,
+        sanc_name: "",
+        anime_id: 0,
+        anime_name: "",
+        url: "",
+        image: "",
+        area_id: 0,
+        area_name: ""
+      }
+    },
+    sanctuaries: {
+      namespaced: true,
+      state: {
+        list: []
+      },
+      mutations: {
+        getList(state, payload) {
+          state.list = payload.list;
+        }
+      },
+      actions: {
+        async getListAction(context) {
+          const payload = {
+            list: []
+          };
+          await axios.get(BASE_URL + "/sanctuaries").then(res => {
+            payload.list = res.data;
+          });
+          context.commit("getList", payload);
+        }
+      }
+    },
+    marker: {
+      namespaced: true,
+      state: {
+        // mapsのmarker内の定義
+        sanc_id: 0,
+        sanc_name: "",
+        anime_id: 0,
+        anime_name: "",
+        image: "",
+        description: "",
+        lat: 0,
+        lng: 0
+      }
+    },
+    maps: {
+      namespaced: true,
+      state: {
+        lat: 0,
+        lng: 0,
+        markers: []
+      },
+      mutations: {
+        getList(state, payload) {
+          state.list = payload.list;
+        }
+      },
+      actions: {
+        async getSanctuaryMapAction(context) {
+          const payload = {
+            list: []
+          };
+          await axios.get(BASE_URL + "/maps").then(res => {
             payload.list = res.data;
           });
           context.commit("getList", payload);
